0606860df5e4ac2afdf325732f78b9ea444541bb
Kordian Bruck <k@bruck.me>
2020-03-22 23:08:49 +0100

    Switch from twitter-node-client to twitter lib, as the old API is deprecated

--- server.js
+++ server.js
@@ -1,37 +1,21 @@
 //Include the node module
-var Twitter = require('twitter-node-client').Twitter;
+var Twitter = require('twitter');
 
 //Get this data from your twitter apps dashboard
-var config = {
-    "consumerKey": "YOUR_CONSUMER_KEY",
-    "consumerSecret": "YOUR_CONSUMER_SECRET",
-    "accessToken": "YOUR_ACCESS_TOKEN",
-    "accessTokenSecret": "YOUR_ACCESS_TOKEN_SECRET",
-    "callBackUrl": "http://localhost:3000/"
-}
-
-//Init the twitter api client with the api key
-var twitter = new Twitter(config);
-
-//Callback functions
-var error = function (err, response, body) {
-    console.log(err)
-};
-var success = function (data) {
-    console.log(data); 
-   
-};
-
+var client = new Twitter({
+    "consumer_key": "YOUR_CONSUMER_KEY",
+    "consumer_secret": "YOUR_CONSUMER_SECRET",
+    "access_token_key": "YOUR_ACCESS_TOKEN",
+    "access_token_secret": "YOUR_ACCESS_TOKEN_SECRET",
+});
 
 //Add members to list via slug/list name 
-twitter.listAddMembers({
-    owner_screen_name:"YOUR_HANDLE",
-    slug:"LIST_NAME_HERE",
-    screen_name:"HANDLE1,HANDLE2,HANDLE3"
-},error,success);
+client.post('lists/members/create_all', {
+  owner_screen_name: "YOUR HANDLE",
+  slug: 'LIST_NAME_HERE',
+  screen_name: "HANDLE1,HANDL2,...",
+},  function(error, response) {
+  if(error) throw error;
+  console.log(response);  // Raw response object.
+});
 
-//Add members to list via list_id
-/*twitter.listAddMembers({
-    list_id:227857378,
-    screen_name:"StartupJuncture,revenantarena,indiegamepr"
-},error,success);*/
